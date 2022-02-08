fa937d6f7f59bea6fd904ef54021d10d5b42fa78
dbmesquita <dionata.bm@gmail.com>
2022-02-08 13:57:32 -0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+const button =document.getElementById("button");
+const toggle
+=document.getElementById("toggle")
+toggle.onclick=function(){
+  button.classList.toggle("active");
+    toggle.classList.toggle("light");
+
+
+}
